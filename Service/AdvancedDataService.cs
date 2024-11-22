9d0d5c448c66da8bcf39ad877bd34d04988342d3
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-11-22 13:09:19 +0700

    Add AdvancedDataService class with async GetData method
    Introduced a new class `AdvancedDataService` in the `DemoDotNet.Service` namespace. This class implements the `IDataService` interface and includes an asynchronous `GetData` method that simulates a 5-second delay before returning "Advanced data service return".

---
+++ Service/AdvancedDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoDotNet.Service
+{
+    public class AdvancedDataService : IDataService
+    {
+        public async Task<string> GetData()
+        {
+            await Task.Delay(5000);
+            return ("Advanced data service return");
+        }
+    }
+}
